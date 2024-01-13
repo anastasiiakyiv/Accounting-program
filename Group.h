df1697eed9999f325e78a269631ed728a47ecbb9
anastasiiakyiv <143424599+anastasiiakyiv@users.noreply.github.com>
2024-01-13 23:45:13 +0200

    Update Group.h

--- Group.h
+++ Group.h
@@ -5,34 +5,34 @@
 
 class Group
 {
-  int number;
-  std::vector<std::shared_ptr<Student>> students;
+    int number;
+    std::vector<std::shared_ptr<Student>> students;
 
 public:
-  Group(int number) : number(number)
-  {
-  }
-  int getNumber() 
-  {
-    return number;
-  }
-  std::vector<std::shared_ptr<Student>> getAllStudents() const
-  {
-    return students;
-  }
+    Group(int number) : number(number)
+    {
+    }
+    int getNumber()
+    {
+        return number;
+    }
+    std::vector<std::shared_ptr<Student>> getAllStudents() const
+    {
+        return students;
+    }
 
-  void addStudentToGroup(std::shared_ptr<Student> student) {
-    // std::cout << "GROUP | addStudentToGroup | student.get()->getLastName() = " << student.get()->getLastName() << std::endl;
-    students.push_back(student);
-    // for (auto _student : students) {
-    // //    std::cout << "GROUP | addStudentToGroup | INLOOP | student.get()->getLastName() = " << _student.get()->getLastName() << std::endl;
-    // }
-  }
+    void addStudentToGroup(std::shared_ptr<Student> student) {
+        // std::cout << "GROUP | addStudentToGroup | student.get()->getLastName() = " << student.get()->getLastName() << std::endl;
+        students.push_back(student);
+        // for (auto _student : students) {
+        // //    std::cout << "GROUP | addStudentToGroup | INLOOP | student.get()->getLastName() = " << _student.get()->getLastName() << std::endl;
+        // }
+    }
 
-  int removeStudentFromGroup(std::shared_ptr<Student> student)
-  {
-    auto it = std::find(students.begin(), students.end(), student);
-      if (it == students.end()) return 0;
-      students.erase(it); return 1;
-  }
+    int removeStudentFromGroup(std::shared_ptr<Student> student)
+    {
+        auto it = std::find(students.begin(), students.end(), student);
+        if (it == students.end()) return 0;
+        students.erase(it); return 1;
+    }
 };
